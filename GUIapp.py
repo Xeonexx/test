3b83a95e7d9fadd610bfffa0e50e46236b1a59a3
Xeonexx <50502156+Xeonexx@users.noreply.github.com>
2019-05-11 18:33:42 +0100

    Updated with better stuffff

--- GUIapp.py
+++ GUIapp.py
@@ -1,65 +1,63 @@
+# Yash Shrimali
+# Gui attempt(s)
+
 import tkinter as tk
 import requests
 
-Height = 500
-Width = 600
+HEIGHT = 500
+WIDTH = 600
 
+def test_function(entry):
+	print("This is the entry:", entry)
 
-def trigger_r(entry):
-    print("This is the entry", entry)  # Used function to check if button's been pressed
-
-# e0fbe9c3531609ff52c56c521b3907ab
 # api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# e0fbe9c3531609ff52c56c521b3907ab
 
+def format_response(weather):
+	try:
+		name = weather['name']
+		desc = weather['weather'][0]['description']
+		temp = weather['main']['temp']
 
-def format_r(weather):
-    try:
-        name = weather['name']
-        desc = weather['weather'][0]['description']
-        temp = weather['main']['temp']
+		final_str = 'City: %s \nConditions: %s \nTemperature (C): %s' % (name, desc, temp)
+	except:
+		final_str = 'There was a problem retrieving that information'
 
-        final_str = 'City: %s \nConditions: %s \nTemperature (C): %s' % (name, desc, temp)
-    except ImportError:
-        final_str = 'There was a problem retrieving that information'
-
-    return final_str
-
+	return final_str
 
 def get_weather(city):
-    weather_key = "e0fbe9c3531609ff52c56c521b3907ab"
-    url = "https://api.openweathermap.org/data/2.5/forecast"
-    params = {"APPID": weather_key, "q": city, "units": "metric"}
-    response = requests.get(url, params=params)
-    weather = response.json()
+	weather_key = 'e0fbe9c3531609ff52c56c521b3907ab'
+	url = 'https://api.openweathermap.org/data/2.5/weather'
+	params = {'APPID': weather_key, 'q': city, 'units': 'metric'}
+	response = requests.get(url, params=params)
+	weather = response.json()
 
-    label['text'] = format_r(weather)
+	label['text'] = format_response(weather)
 
 
-root = tk.Tk()  # This is to make the actual blank border window. Places everything into this window
 
-canvas = tk.Canvas(root, height=Height, width=Width)
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-background_image = tk.PhotoImage(file="weathergui.png")
+background_image = tk.PhotoImage(file='landscape.png')
 background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)  # Wanted the picture to take up all the space.
+background_label.place(relwidth=1, relheight=1)
 
-frame = tk.Frame(root, bg="#90d1fe", bd=5)
-frame.place(relwidth=0.75, relheight=0.1, relx=0.5, rely=0.1, anchor="n")  # Sets relative height/width for the frame. The numbers represent the % of the screen it covers. Used anchor=n to place relx at the top for 0.5.
+frame = tk.Frame(root, bg='#90d1ef', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
 entry = tk.Entry(frame, font=40)
 entry.place(relwidth=0.65, relheight=1)
 
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
 
-button = tk.Button(frame, text="Get Weather", font=10, command=lambda: get_weather(entry.get()))  # To create button. Like a nested structure where the root creates everything
-button.place(relx=0.7, relwidth=0.3, relheight=1)  # can use either place, pack or grid. Place seems to be the most easiest to use for me.
-# set relx to 0.7 as the entry width is 0.65 so to create space i chose 0.7
-
-lower_frame = tk.Frame(root, bg="#90d1fe", bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
-# Used relx=0.5 so that this frame which is going to output data is centered, also made the anchor north for this to happen
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.6, relheight=0.5, anchor='n')
 
 label = tk.Label(lower_frame)
-label.place(relwidth=1, relheight=1)  # This label is going to be used to output data, hence made it the whole size of the lower frame
+label.place(relwidth=1, relheight=1)
 
-root.mainloop()  # runs application
+root.mainloop()
