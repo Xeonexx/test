2b0e7b0c5b53bd95f10b59473a50a8c655d38849
Xeonexx <50502156+Xeonexx@users.noreply.github.com>
2019-05-12 01:30:09 +0100

    Even better better version

--- GUIapp.py
+++ GUIapp.py
@@ -4,14 +4,17 @@
 import tkinter as tk
 import requests
 
-HEIGHT = 500
-WIDTH = 600
+Height = 500
+Width = 800
+
 
 def test_function(entry):
 	print("This is the entry:", entry)
 
+
 # api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
 # e0fbe9c3531609ff52c56c521b3907ab
+
 
 def format_response(weather):
 	try:
@@ -25,39 +28,55 @@ def format_response(weather):
 
 	return final_str
 
+
 def get_weather(city):
 	weather_key = 'e0fbe9c3531609ff52c56c521b3907ab'
 	url = 'https://api.openweathermap.org/data/2.5/weather'
-	params = {'APPID': weather_key, 'q': city, 'units': 'metric'}
-	response = requests.get(url, params=params)
+	param = {'APPID': weather_key, 'q': city, 'units': 'metric'}
+	response = requests.get(url, params=param)
 	weather = response.json()
 
 	label['text'] = format_response(weather)
 
 
-
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas = tk.Canvas(root, height=Height, width=Width)
 canvas.pack()
 
 background_image = tk.PhotoImage(file='landscape.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-frame = tk.Frame(root, bg='#90d1ef', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+frame = tk.Frame(root, bg='#90d1ef', bd=10)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')  # The search bar frame
+
+
+lower_frame = tk.Frame(root, bg='#90d1ef', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.6, relheight=0.5, anchor='n')  # The output frame
+
+
+even_lower_frame = tk.Frame(root, bg='green', bd=3)
+even_lower_frame.place(relx=0.01, rely=0.94, relwidth=0.25, relheight=0.05)  # Credits frame
+
+
+label2 = tk.Label(even_lower_frame, text='Made by: Yash Shrimali', bg='cyan')  # Label for even_lower_frame
+label2.place(relwidth=1, relheight=1)
 
 entry = tk.Entry(frame, font=40)
-entry.place(relwidth=0.65, relheight=1)
+entry.place(relwidth=0.65, relheight=1)  # Enables the search bar and makes it interactable
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Weather", font=80, command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
-lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.6, relheight=0.5, anchor='n')
 
-label = tk.Label(lower_frame)
-label.place(relwidth=1, relheight=1)
+label = tk.Label(lower_frame, bg='#f3ff87')
+label.place(relwidth=1, relheight=1)  # for output frame
+label.config(font=40)
+
+weather_icon = tk.Canvas(label, bd=0, highlightthickness=0)
+weather_icon.place(relx=0.9, rely=0, relwidth=1, relheight=0.2)
+
 
 root.mainloop()
